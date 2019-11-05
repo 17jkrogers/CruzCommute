3b8845b419130a15b17b767afb9d4c8724c2ca73
jakroger <jakroger@ucsc.edu>
2019-11-05 15:42:09 -0800

    Finished barebones custom listview.

--- app/src/main/java/com/group25/cruzcommute/RouteAdapter.java
+++ app/src/main/java/com/group25/cruzcommute/RouteAdapter.java
@@ -1,11 +1,43 @@
 package com.group25.cruzcommute;
 
-import android.app.ListActivity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-public class RouteAdapter extends ListActivity {
+import java.util.ArrayList;
 
-    public RouteAdapter(){
-        super(ScheduleActivity.this, R.layout.row, )
+public class RouteAdapter extends ArrayAdapter<Route> {
+
+    private ArrayList<Route> routeList;
+    private Context schedctx;
+    private int layoutId;
+
+    public RouteAdapter(Context ctx, int rId, ArrayList<Route> routes){
+        super(ctx, rId, routes);
+        schedctx = ctx;
+        layoutId = rId;
+        routeList = routes;
     }
 
+    @Override
+    public View getView(final int pos, View view, ViewGroup viewG){
+        LayoutInflater layoutInflater = LayoutInflater.from(schedctx);
+
+        View mView = layoutInflater.inflate(layoutId, null, false);
+
+        TextView routeNum = mView.findViewById(R.id.routeNumberText);
+        TextView routeSlow = mView.findViewById(R.id.routeSlowdown);
+        TextView routeCong = mView.findViewById(R.id.routeCongestion);
+
+        Route route = routeList.get(pos);
+
+        routeNum.setText(route.getRouteNum());
+        routeSlow.setText("" + route.getSlowdown());
+        routeCong.setText(route.getCongestion());
+
+        return mView;
+    }
 }
--- app/src/main/java/com/group25/cruzcommute/ScheduleActivity.java
+++ app/src/main/java/com/group25/cruzcommute/ScheduleActivity.java
@@ -3,6 +3,9 @@ package com.group25.cruzcommute;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ScheduleActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class ScheduleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+
+        ArrayList routeList = new ArrayList<Route>();
+        ListView routeView = findViewById(R.id.routeList);
+
+        routeList.add(new Route("16", "High", 25));
+        routeList.add(new Route("15", "Low", -5));
+        routeList.add(new Route("19", "Medium", 5));
+
+        RouteAdapter adapter = new RouteAdapter(this, R.layout.route_list, routeList);
+
+        routeView.setAdapter(adapter);
     }
 }
