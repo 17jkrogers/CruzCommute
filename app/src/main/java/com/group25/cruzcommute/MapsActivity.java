25c8811aca516c4986b23157d71ecd5acec92643
jakroger <jakroger@ucsc.edu>
2019-11-05 14:54:00 -0800

    Working on creating a custom adapter for a listview.

--- app/src/main/java/com/group25/cruzcommute/MapsActivity.java
+++ app/src/main/java/com/group25/cruzcommute/MapsActivity.java
@@ -41,10 +41,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Santa Cruz and move the camera
+        LatLng santa_cruz = new LatLng(36.974117, -122.030792);
+        mMap.addMarker(new MarkerOptions().position(santa_cruz).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(santa_cruz));
     }
 
     public void onSettingsClicked(View v){
---
+++ app/src/main/java/com/group25/cruzcommute/RouteAdapter.java
@@ -0,0 +1,11 @@
+package com.group25.cruzcommute;
+
+import android.app.ListActivity;
+
+public class RouteAdapter extends ListActivity {
+
+    public RouteAdapter(){
+        super(ScheduleActivity.this, R.layout.row, )
+    }
+
+}
