1998ee6d3ec307d3e4ff6876bc7d4c8bd65052be
jakroger <jakroger@ucsc.edu>
2019-11-19 15:05:08 -0800

    Added basic Firebase functionality to the ReportActivity.

--- app/src/main/java/com/group25/cruzcommute/ReportActivity.java
+++ app/src/main/java/com/group25/cruzcommute/ReportActivity.java
@@ -3,10 +3,19 @@ package com.group25.cruzcommute;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -32,12 +41,35 @@ public class ReportActivity extends AppCompatActivity {
         routeNums.add(4, 20);
         routeNums.add(5, 22);
 
+
+
         adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, routeNums);
 
         routeSpinner.setAdapter(adapter);
     }
 
     public void onReportClicked(View view) {
+        String routeStr = routeSpinner.getSelectedItem().toString();
+        int route = Integer.parseInt(routeStr);
+
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        final DatabaseReference ref = db.getReference("route" + route);
+
+        RadioGroup selector = findViewById(R.id.congestionGroup);
+        RadioButton selected = findViewById(selector.getCheckedRadioButtonId());
+        final String congLevel = selected.getText().toString();
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String oldVal = dataSnapshot.getValue(String.class);
+                ref.setValue(congLevel + oldVal.substring(oldVal.indexOf(",")));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError dbErr) {
+            }
+        });
+
         Toast.makeText(this, "Thank you for submitting your report!", Toast.LENGTH_LONG).show();
     }
 
