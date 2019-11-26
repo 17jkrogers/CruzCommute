2dd3d1748afde0f1730e5109ec26af9569a10a3d
17jkrogers <rogersson42@gmail.com>
2019-11-25 22:31:56 -0800

    Fixed Nisha's best code ever.

--- app/src/main/java/com/group25/cruzcommute/ScheduleActivity.java
+++ app/src/main/java/com/group25/cruzcommute/ScheduleActivity.java
@@ -59,47 +59,14 @@ public class ScheduleActivity extends AppCompatActivity {
                     routeView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                         @Override
                         public void onItemClick(AdapterView<?> adapterView, View view, int position, long arg) {
-                            if (currentDay == 1 || currentDay == 7) {
-                                if (position == 0) {
-                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/16/we"));
-                                    startActivity(intent);
-                                } else if (position == 1) {
-                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/15/we"));
-                                    startActivity(intent);
-                                } else if (position == 2) {
-                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/19/we"));
-                                    startActivity(intent);
-                                } else if (position == 3) {
-                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/10/we"));
-                                    startActivity(intent);
-                                } else if (position == 4) {
-                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/20/we"));
-                                    startActivity(intent);
-                                } else {
-                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/22/we"));
-                                    startActivity(intent);
-                                }
+
+                            if(currentDay == 1 || currentDay == 7) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/" + routeList.get(position).getRouteNum() + "/we"));
+                                startActivity(intent);
                             }
                             else{
-                                if (position == 0) {
-                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/16/wd"));
-                                    startActivity(intent);
-                                } else if (position == 1) {
-                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/15/wd"));
-                                    startActivity(intent);
-                                } else if (position == 2) {
-                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/19/wd"));
-                                    startActivity(intent);
-                                } else if (position == 3) {
-                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/10/wd"));
-                                    startActivity(intent);
-                                } else if (position == 4) {
-                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/20/wd"));
-                                    startActivity(intent);
-                                } else {
-                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/22/wd"));
-                                    startActivity(intent);
-                                }
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/" + routeList.get(position).getRouteNum() + "/wd"));
+                                startActivity(intent);
                             }
                         }
                     });
