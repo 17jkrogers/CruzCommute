4102ad6fb22146d310e613282fc92e383a7e0a50
17jkrogers <rogersson42@gmail.com>
2019-11-25 22:28:04 -0800

    Added functionality to dynamically populate schedule view from Firebase. Changed the route_list.xml layout to better accomodate.

--- app/src/main/java/com/group25/cruzcommute/ScheduleActivity.java
+++ app/src/main/java/com/group25/cruzcommute/ScheduleActivity.java
@@ -5,10 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.Calendar;
+import java.util.Map;
 import java.util.TimeZone;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -16,77 +25,105 @@ import java.util.Calendar;
 
 public class ScheduleActivity extends AppCompatActivity {
 
+    private FirebaseDatabase db;
+    private ArrayList<Route> routeList;
+    ListView routeView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
 
-        ArrayList routeList = new ArrayList<Route>();
-        ListView routeView = findViewById(R.id.routeList);
-        Calendar busCalendar = Calendar.getInstance(TimeZone.getDefault());
-        final int currentDay = busCalendar.get(Calendar.DAY_OF_WEEK);
-        routeList.add(new Route("16", "High", 25));
-        routeList.add(new Route("15", "Low", -5));
-        routeList.add(new Route("19", "Medium", 5));
-        routeList.add(new Route( "10", "Medium", 10));
-        routeList.add(new Route( "20", "High", 5));
-        routeList.add(new Route( "22", "High", 15));
+        routeList = new ArrayList<Route>();
 
-        routeView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        db = FirebaseDatabase.getInstance();
+        final DatabaseReference dbRef = db.getReference();
+
+        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long arg) {
-                if (currentDay == 1 || currentDay == 7) {
-                    if (position == 0) {
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/16/we"));
-                        startActivity(intent);
-                    } else if (position == 1) {
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/15/we"));
-                        startActivity(intent);
-                    } else if (position == 2) {
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/19/we"));
-                        startActivity(intent);
-                    } else if (position == 3) {
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/10/we"));
-                        startActivity(intent);
-                    } else if (position == 4) {
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/20/we"));
-                        startActivity(intent);
-                    } else {
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/22/we"));
-                        startActivity(intent);
-                    }
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Route added;
+                Map<String, Object> vals = (Map<String, Object>) dataSnapshot.getValue();
+
+                for(Map.Entry<String, Object> value: vals.entrySet()){
+                    String routeNum = value.getKey().substring(value.getKey().indexOf("e") + 1);
+                    String congLevel = value.getValue().toString().substring(0, value.getValue().toString().indexOf(","));
+                    int slowdown = Integer.parseInt(value.getValue().toString().substring(value.getValue().toString().indexOf(",") + 2));
+                    added = new Route(routeNum, congLevel, slowdown);
+                    routeList.add(added);
+
+                    routeView = findViewById(R.id.routeList);
+                    Calendar busCalendar = Calendar.getInstance(TimeZone.getDefault());
+                    final int currentDay = busCalendar.get(Calendar.DAY_OF_WEEK);
+
+                    routeView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> adapterView, View view, int position, long arg) {
+                            if (currentDay == 1 || currentDay == 7) {
+                                if (position == 0) {
+                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/16/we"));
+                                    startActivity(intent);
+                                } else if (position == 1) {
+                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/15/we"));
+                                    startActivity(intent);
+                                } else if (position == 2) {
+                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/19/we"));
+                                    startActivity(intent);
+                                } else if (position == 3) {
+                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/10/we"));
+                                    startActivity(intent);
+                                } else if (position == 4) {
+                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/20/we"));
+                                    startActivity(intent);
+                                } else {
+                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/22/we"));
+                                    startActivity(intent);
+                                }
+                            }
+                            else{
+                                if (position == 0) {
+                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/16/wd"));
+                                    startActivity(intent);
+                                } else if (position == 1) {
+                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/15/wd"));
+                                    startActivity(intent);
+                                } else if (position == 2) {
+                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/19/wd"));
+                                    startActivity(intent);
+                                } else if (position == 3) {
+                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/10/wd"));
+                                    startActivity(intent);
+                                } else if (position == 4) {
+                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/20/wd"));
+                                    startActivity(intent);
+                                } else {
+                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/22/wd"));
+                                    startActivity(intent);
+                                }
+                            }
+                        }
+                    });
+
+
+                    RouteAdapter adapter = new RouteAdapter(ScheduleActivity.this, R.layout.route_list, routeList);
+                    routeView.setAdapter(adapter);
                 }
-                else{
-                    if (position == 0) {
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/16/wd"));
-                        startActivity(intent);
-                    } else if (position == 1) {
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/15/wd"));
-                        startActivity(intent);
-                    } else if (position == 2) {
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/19/wd"));
-                        startActivity(intent);
-                    } else if (position == 3) {
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/10/wd"));
-                        startActivity(intent);
-                    } else if (position == 4) {
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/20/wd"));
-                        startActivity(intent);
-                    } else {
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scmtd.com/en/routes/schedule/20201/22/wd"));
-                        startActivity(intent);
-                    }
-                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError dbErr) {
             }
         });
 
 
-        RouteAdapter adapter = new RouteAdapter(this, R.layout.route_list, routeList);
-        routeView.setAdapter(adapter);
     }
 
     public void transferReportActivity(View view) {
         Intent intent = new Intent(this, ReportActivity.class);
         startActivity(intent);
+    }
+
+    public void addToList(Route added){
+        routeList.add(added);
     }
 }
